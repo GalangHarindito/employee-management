c860f38c71c9afbd07fa37006027b994b4de1bf9
GAlangHarindito <galang.harindito@gmail.com>
2021-04-05 20:01:43 +0700

    delete console.log

--- src/app/Directive/currency.directive.ts
+++ src/app/Directive/currency.directive.ts
@@ -27,7 +27,6 @@ export class CurrencyDirective implements OnInit {
   format(val:string) {
     // 1. test for non-number characters and replace/remove them
     const numberFormat = parseInt(String(val).replace(this.currencyChars, ''));
-     console.log(numberFormat); // raw number
 
     // 2. format the number (add commas)
     const usd = this.decimalPipe.transform(numberFormat, '1.0', 'en-US');
